88d600cb27a14c96e1cd2694a3554c9b21dab33a
korniplodiks <korniplodiks@users.noreply.github.com>
2025-11-22 09:21:41 +0200

    feat: fix compilation error

--- src/api.js
+++ src/api.js
@@ -212,3 +212,30 @@ class API {
 
 // Update 88
 module.exports = API;
+
+
+// API module for LampServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 89
+module.exports = API;
