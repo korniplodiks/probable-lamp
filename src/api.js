5c0d0de8c5a96fecbb63cf057ac4f8b5a4406417
korniplodiks <korniplodiks@users.noreply.github.com>
2025-12-20 04:33:43 +0200

    update: security enhancement

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 55
 module.exports = API;
+
+
+// API module for LampServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 68
+module.exports = API;
